bac53500cf35cc3b4b99ae769a640f0e4f56f89b
mimccom <mimccom@okstate.edu>
2022-10-12 13:12:56 -0500

    player and camera movement
    player movement
    camera movement

---
+++ Assets/Scripts/playerMovement.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    private Rigidbody2D body;
+    private Animator anim;
+    private bool grounded;
+
+    private void Awake()
+    {
+        body = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        body.velocity = new Vector2(horizontalInput * speed, body.velocity.y);
+
+        //flip the player when they change direction
+        if (horizontalInput > 0.1f)
+        {
+            transform.localScale = new Vector3(2,2,2);
+        } else if (horizontalInput < -0.1f)
+        {
+            transform.localScale = new Vector3(-2, 2, 2);
+        }
+        if (Input.GetKey(KeyCode.Space) && grounded)
+        {
+            Jump();
+        }
+
+
+        //set animator parameters
+        anim.SetBool("running", horizontalInput !=0);
+        anim.SetBool("grounded", grounded);
+    }
+
+    private void Jump()
+    {
+        body.velocity = new Vector3(body.velocity.x, speed);
+        anim.SetTrigger("Jump");
+        grounded = false;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Ground")
+        {
+            grounded = true;
+        }
+    }
+}

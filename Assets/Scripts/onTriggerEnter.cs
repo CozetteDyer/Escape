3705da42572e5f25c2ac847dc0ed12363fb01c09
ICook094 <icook@okstate.edu>
2022-10-16 23:07:13 -0500

    Add exit script and skybox to all scenes in use

--- Assets/Scripts/onTriggerEnter.cs
+++ Assets/Scripts/onTriggerEnter.cs
@@ -5,15 +5,12 @@ using UnityEngine.SceneManagement;
 
 public class onTriggerEnter : MonoBehaviour
 {
-    public string characterTag;
-    public Object sceneToLoad;
-
     //Upon collision with another GameObject, this GameObject will reverse direction
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.CompareTag(characterTag))
+        if (collision.CompareTag("Door"))
         {
-            SceneManager.LoadScene(sceneToLoad.name);
+            SceneManager.LoadScene("2nd room");
         }
     }
 }
